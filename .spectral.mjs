3590b7bf4760e3ef6dce4be668671a035b4dab13
Carlos Cabral <carlosmscabral@gmail.com>
2023-02-02 09:16:08 -0300

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE4MTk4NA/spectral.js?branch_slug=main&token=285e7216-6c7c-4e1d-861f-064e681b11df";
+export default { extends: ruleset };
